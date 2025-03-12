51765af472bd0dc9a73649ffa0820dd21843238f
Fortuna233 <1311722974@qq.com>
2025-03-12 22:22:38 +0800

    ex17 first commit

---
+++ ex17.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address 
+{
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_ROWS];
+};
+
+struct Database
+{
+	struct Address rowx[MAX_ROWS];
+};
+
+
