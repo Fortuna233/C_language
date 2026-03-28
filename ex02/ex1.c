72dd59e1d3301aab4f25e14a04e836d3fcaaa6cc
Fortuna233 <1311722974@qq.com>
2026-03-28 14:40:52 +0800

    finish ex04

---
+++ ex02/ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(int argc, char *argv[])  //删除括号中内容，对结果无影响
+{
+	puts("Hello world.");
+	
+	puts("wuhu");
+	puts("wuhu");
+	puts("wuhu");
+	puts("wuhu");
+	return 0;      //删除本行对结果无影响
+}
