edd2f566d35b6bbf36babe3c54be9b441768ccb1
Fortuna233 <1311722974@qq.com>
2026-04-07 11:15:33 +0800

    finish ex08

--- ex08/ex8.c
+++ ex08/ex8.c
@@ -6,23 +6,26 @@ char full_name[] = {'Z', 'e', 'd', ' ', 'A', '.', ' ', 'S', 'h', 'a', 'w'};
 int main(int agrc, char *argv[])
 {
 	int areas[] = {10, 12, 13, 14, 20};
+	// T1
+	areas[0] = 100;
+	areas[1] = 10;
 	char name[] = "Zed";
+	// T2
+	name[0] = 'w';
+	// T3
+	name[1] = areas[1] + 32;
 
 	printf("%ld\n", sizeof(int));
 	printf("%ld\n", sizeof(areas) / sizeof(int));
 	printf("%d, %d\n", areas[0], areas[1]);
+	printf("%d, %d\n", areas[10], areas[1]);
 	printf("The size of a char: %ld\n", sizeof(char));
-  printf("The size of name (char[]): %ld\n",
-            sizeof(name));
-  printf("The number of chars: %ld\n",
-            sizeof(name) / sizeof(char));
+	printf("The size of name (char[]): %ld\n", sizeof(name));
+	printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
 
-  printf("The size of full_name (char[]): %ld\n",
-            sizeof(full_name));
-  printf("The number of chars: %ld\n",
-            sizeof(full_name) / sizeof(char));
+	printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
+	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
 
-  printf("name=\"%s\" and full_name=\"%s\"\n",
-            name, full_name);
+	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
 	 return 0;
 }
