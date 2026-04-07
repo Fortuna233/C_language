01f4899994f8146ee163ed6f68a91be5a5dff9eb
Fortuna233 <1311722974@qq.com>
2026-04-07 12:52:27 +0800

    finish ex09

--- ex09/ex9.c
+++ ex09/ex9.c
@@ -29,11 +29,39 @@ int main(int agrc, char *argv[])
 	
 	int A[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
-	printf("%d\n", A[0]);
 	for(int i = 0; i < 10; i++)
 	{
 		printf("%d: %d\n", i, A[i]);
 	}
+
+	// T1&T2
+	int ex_numbers[4] = {0};
+	ex_numbers[0] = 'a';
+	printf("ex_numbers[0]: %d\n", ex_numbers[0]);
+	printf("size_of_int: %ld\n", sizeof(int));
+	char ex_name[] = "WUH";
+	ex_name[0] = 64;
+	printf("ex_name[0]: %c\n", ex_name[0]);
+	
+	// T3
+	printf("%c\n", name[0]);
+	putchar(name[0]);
+	printf("\n");
+	fputc(name[0], stdout);
+	printf("\n");
+
+	// T4 Z:5A E:45 D:44 \0:00
+	// -> 0044455A
+	int whole = *(int *)name;
+	printf("%d\n", whole);
+	printf("%#x\n", whole);
+	// T6
+	// 指针指向只读常量区，无法更改
+	/*
+	char *ex_name = "WUH";
+	ex_name[0] = 1;
+	printf("ex_name[0]: %c", ex_name[0]);
+	*/
 
 	return 0;
 }
