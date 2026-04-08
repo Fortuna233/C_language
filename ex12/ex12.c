abee44520baadf60aeabb805853e4bf6c149be09
Fortuna233 <1311722974@qq.com>
2026-04-08 15:39:11 +0800

    finish ex12

--- ex12/ex12.c
+++ ex12/ex12.c
@@ -2,11 +2,14 @@
 
 int main(int argc, char *argv[]) {
 
-	if(argc == 2)
+	/*
+	if(argc == 1)
 	{
 		printf("one argument\n");
+	// T4
+	// printf("no argument\n");
 	}
-	else if(argc > 2 && argc < 5)
+	else if(argc > 1 && argc < 4)
 	{
 		printf("arguments:\n");
 
@@ -20,8 +23,25 @@ int main(int argc, char *argv[]) {
 	{
 		printf("too many arguments\n");
 	}
+	*/
+	// T3
+	int i = 0;
+	while(i < 10) {
+		printf("argv[%d]: %s\n", i, argv[i]);
+		i++;
+		if (i == 5) {
+			break;
+		}
+	}
 
 	return 0;
+	/* 
+	&&：逻辑与（两边都为真，结果才真）
+	||：逻辑或（一边为真，结果就真）
+	!：逻辑非（真变假，假变真）
+	关系运算符（比较大小）
+	== 等于、!= 不等于、< 小于、> 大于、<= 小于等于、>= 大于等于
+	*/
 }
 
 
